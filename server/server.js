452e5b7c0508f6ec156fa0b9b8d7b2dfddc32d97
DaDDy <blacklistuser420@gmail.com>
2022-12-06 17:55:18 +0630

    test-cc

--- server/server.js
+++ server/server.js
@@ -1,9 +1,9 @@
-require("dotenv").config({ path: __dirname + ".env" });
+require("dotenv").config({ path: __dirname + "/.env" });
 const http = require("http");
 const app = require("./app");
 const server = http.createServer(app);
 const PORT = process.env.PORT;
-
+console.log(process.env);
 server.listen(PORT, () => {
   console.log(`server is running on ${PORT}`);
 });
